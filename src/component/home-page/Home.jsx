9a25fec1c08e5a2605b69c16d9f0ab86f1382c46
WaelZ01 <149936969+WaelZ01@users.noreply.github.com>
2024-11-03 01:52:29 +0300

    fix

--- src/component/home-page/Home.jsx
+++ src/component/home-page/Home.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 export default function Home() {
   return (
     <>
+        <div className='home-container'>
+          <h1 className='homePage-header'>Welcome To Aroura :</h1>
+            <p className='homePage-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Nesciunt commodi architecto rerum sapiente quidem! Placeat quis exercitationem 
+              consectetur laborum ipsum rerum. Dolorem natus doloremque non, repellendus quod quasi eaque rem?
+            </p>
+        </div>
 
-    <h1>What is Aroura</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta hic, corrupti atque doloremque,
-         molestiae accusantium recusandae, reiciendis molestias
-         quibusdam optio fugit odio nostrum quaerat itaque laboriosam similique ipsam. Distinctio, est.
-         </p>
-    
     </>
   )
 }
--- src/component/shop/ShopPage.jsx
+++ src/component/shop/ShopPage.jsx
@@ -49,6 +49,7 @@ export default function ShopPage() {
       </div>
 
 
+
     
 
     </div>
